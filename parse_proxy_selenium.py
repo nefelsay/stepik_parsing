a899849fdf85b36119d41a14172a1f4473857b12
nefelsay <65243638+nefelsay@users.noreply.github.com>
2023-01-13 13:11:25 +0300

    Update parse_proxy_selenium.py

--- parse_proxy_selenium.py
+++ parse_proxy_selenium.py
@@ -19,24 +19,23 @@ class Parser:
             if el:
                 for x in range(1, last_pagen + 1):
                     try:
-                        self.source_page.append(browser.page_source)
+                        source = browser.page_source
                         a = browser.find_element(By.CLASS_NAME, 'next_array').find_element(By.TAG_NAME, 'a')
                         browser.execute_script("window.scrollBy(0,1000)")
                         WebDriverWait(browser, 10).until(EC.element_to_be_clickable(a)).click()
                         print(f'Страница {x} обработана')
-                        for link in self.source_page:
-                            soup = BeautifulSoup(link, 'lxml')
-                            ips = soup.find('table').find_all('tr')
-                            port = soup.find('tbody').find_all('tr')
-                            for ip, port in zip(ips, port):
-                                g = f"{ip.find_all('td')[0].text}:{port.find_all('td')[1].text}\n"
-                                print('Прокси сохранён в файл',g,end='')
-                                file = open('proxy_list.txt', 'a')
-                                file.write(g)
-                            file.close()
+                        soup = BeautifulSoup(source, 'lxml')
+                        ips = soup.find('table').find_all('tr')
+                        port = soup.find('tbody').find_all('tr')
+                        for ip, port in zip(ips, port):
+                            # print(ip,port)
+                            g = f"{ip.find_all('td')[0].text}:{port.find_all('td')[1].text}\n"
+                            print('Прокси сохранён в файл',g, end='')
+                            file = open('proxy_list.txt', 'a')
+                            file.write(g)
+                        file.close()
                     except Exception:
                         continue
-
 
     def main(self):
         self.open_chrome()
